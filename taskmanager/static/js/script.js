cdc4d334769c513097f0cf1c18775eba907f3c3f
cmcdgit <cmcdgit@gmail.com>
2025-02-14 14:26:26 +0000

    Added editing functionlity with preloaded data

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // sidenav initialization
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  var datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" },
+  });
+
+  // select initialization
+  var selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  // collapsible initialization
+  var collapsible = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsible);
 });
